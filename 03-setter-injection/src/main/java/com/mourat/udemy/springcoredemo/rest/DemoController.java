141840247d341354095da6961dd1325f635cebde
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-26 18:04:28 +0300

    Added setter injection training material

---
+++ 03-setter-injection/src/main/java/com/mourat/udemy/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.mourat.udemy.springcoredemo.rest;
+
+import com.mourat.udemy.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //A private field for the dependency
+    private Coach myCoach;
+
+    //A setter for dependency injection
+    @Autowired
+    public void setCoach(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
